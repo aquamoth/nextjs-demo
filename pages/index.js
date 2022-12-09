1faf920524b55b9719622924254ea0df326e5829
Mattias Åslund <mattias.aslund@shoppa.com>
2022-12-09 08:55:41 +0100

    Showing Linus

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Next to Vercel demo with Jonas!
+          Next to Vercel demo with Linus!
         </h1>
 
         <p className={styles.description}>
